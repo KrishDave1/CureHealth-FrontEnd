de0a244b9bf65431dfe57aff564144c0fb0fbadb
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-10-24 00:45:26 +0530

    Updated Footer

---
+++ CureHealth-FrontEnd/src/components/Footer.jsx
@@ -0,0 +1,94 @@
+/** @format */
+
+import { SocialIcon } from "react-social-icons";
+
+const Footer = () => {
+  var today = new Date();
+  return (
+    <>
+      <footer className='bg-[#F4F7FB] text-[#4f585e] text-center fixed bottom-0 w-full'>
+        <div className='footer-wrapper flex justify-evenly py-5 px-2 bg-gradient-to-t from-slate-300 to-slate-400'>
+          <div className='footer-legal-info'>
+            <div className='footer-section'>
+              <h3 className='text-[#4f585e] text-2xl font-semibold mb-3'>
+                Legal Information
+              </h3>
+              <ul className='text-xl text-left'>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/terms'>Terms of Use</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/privacy'>Privacy Policy</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/cookies'>Cookies Policy</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/return'>Return Policy</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='footer-company-info'>
+            <div className='footer-section'>
+              <h3 className='text-[#4f585e] text-2xl font-semibold mb-3'>Company</h3>
+              <ul className='text-xl text-left'>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/about'>About Us</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/blog'>Blog</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/careers'>Careers</a>
+                </li>
+                <li className='hover:text-gray-500 mb-2 transition ease-linear'>
+                  <a href='/testimonials'>Testimonials</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='footer-connect'>
+            <div className='footer-section'>
+              <h3 className='text-[#4f585e] text-2xl font-semibold mb-8'>
+                Connect with Us
+              </h3>
+              <ul>
+                <div style={{ padding: "10px", display: "inline" }}>
+                  <SocialIcon
+                    style={{ height: "40px", width: "40px", margin: "10px" }}
+                    url='https://instagram.com'
+                  />
+                </div>
+                <div style={{ padding: "10px", display: "inline" }}>
+                  <SocialIcon
+                    style={{ height: "40px", width: "40px" }}
+                    url='https://facebook.com'
+                  />
+                </div>
+                <div style={{ padding: "10px", display: "inline" }}>
+                  <SocialIcon
+                    style={{ height: "40px", width: "40px" }}
+                    url='https://twitter.com'
+                  />
+                </div>
+                <div style={{ padding: "10px", display: "inline" }}>
+                  <SocialIcon
+                    style={{ height: "40px", width: "40px" }}
+                    url='https://whatsapp.com'
+                  />
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='footer-copyright bg-slate-300 text-sm
+        '>
+          &copy; {today.getFullYear()} CureHealth. All Rights Reserved
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ CureHealth-FrontEnd/src/context.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React, { useContext } from "react";
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+};
+export { AppContext, AppProvider };
