48801e35e103ddc616b2f14c80f231a1680d1567
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-10-26 12:37:03 +0530

    Removed unnecessary files

--- CureHealth-FrontEnd/src/_codux/boards/video-calling/video-calling.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import VideoCalling from '../../../components/VideoCalling.jsx';
-
-export default createBoard({
-    name: 'VideoCalling',
-    Board: () => <VideoCalling />,
-    isSnippet: true,
-});
