19b6fc1005fb995cfa0085b3205103f64a342be4
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-10-27 14:00:51 +0530

    Image added to public folder

--- CureHealth-FrontEnd/src/pages/LoginPage.jsx
+++ CureHealth-FrontEnd/src/pages/LoginPage.jsx
@@ -5,15 +5,36 @@ const LoginPage = () => {
     const { contextData } = useGlobalContext();
 
   return (
-      <div>
+    <>
+      <div className='login-page'>
+        <h1>Login Page</h1>
+      </div>
+      <div className='flex justify-evenly'>
+        <div className=''>
           <form onSubmit={contextData.loginUser}>
-              <input type="text" name="username"
-                  placeholder="Enter your username" />
-                <input type="password" name="password" placeholder="Enter your password"/>
-                <input type="submit" value="Login to your account" className="btn"/>
+            <input
+              type='text'
+              name='username'
+              placeholder='Enter your username'
+            />
+            <input
+              type='password'
+              name='password'
+              placeholder='Enter your password'
+            />
+            <input
+              type='submit'
+              value='Login to your account'
+              className='btn'
+            />
           </form>
-    </div>
-  )
+        </div>
+        <div className=' h-96'>
+          <img src='../../Image.avif' alt='Image' className='object-cover'/>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage
