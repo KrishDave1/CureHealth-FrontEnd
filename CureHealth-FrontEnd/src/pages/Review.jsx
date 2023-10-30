b4faae238a3bab12d621e0fc727f48b7877487f7
Vaibhav <mittalvaibhav277@gmail.com>
2023-10-31 01:34:29 +0530

    completed urgent care section

--- CureHealth-FrontEnd/src/pages/Review.jsx
+++ CureHealth-FrontEnd/src/pages/Review.jsx
@@ -10,31 +10,33 @@ const Review = ({ name, description, rating, url }) => {
 
   return (
     <div
-      className={`bg-blue-200 m-5 rounded-lg  ${read ? "h-[350px]" : "h-[290px]"} p-5`}
+      className={`bg-blue-200 m-5 rounded-lg  ${
+        read ? "h-[350px]" : "h-[290px]"
+      } p-5`}
     >
-      <div className='flex flex-col justify-evenly'>
-        <div className='flex justify-evenly items-center'>
+      <div className="flex flex-col justify-evenly">
+        <div className="flex justify-evenly items-center">
           {" "}
           <img
-            className='w-16 h-16 rounded-full'
+            className="w-16 h-16 rounded-full"
             src={url}
-            alt='person image'
+            alt="person image"
           />
           {rating == 4 ? (
-            <p className='text-xl'>⭐️⭐️⭐️⭐️</p>
+            <p className="text-xl">⭐️⭐️⭐️⭐️</p>
           ) : (
-            <p className='text-xl'>⭐️⭐️⭐️⭐️⭐️</p>
+            <p className="text-xl">⭐️⭐️⭐️⭐️⭐️</p>
           )}
         </div>
         <div>
           {" "}
-          <p className='text-md font-light mt-4'>{info}</p>
+          <p className="text-md font-light mt-4">{info}</p>
           <button onClick={() => setRead(!read)}>
             {read ? "Read Less" : "Read More"}
           </button>
         </div>
         <div>
-          <p className='text-lg font-light mt-3'>~{name}</p>
+          <p className="text-lg font-light mt-3">~{name}</p>
         </div>
       </div>
     </div>
