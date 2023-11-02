bcbf843baafb1a96763e11cf435fea4fcd6552c0
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-11-02 18:14:30 +0530

    Update in chat section

--- CureHealth-FrontEnd/src/pages/Chatroom.jsx
+++ CureHealth-FrontEnd/src/pages/Chatroom.jsx
@@ -1,7 +1,10 @@
 import React,{useEffect,useState} from "react";
 import './Chatroom.css';
+import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from 'react-chat-engine-advanced'
+import { useGlobalContext } from "../context";
 
 const Chatroom = () => {
+  const { ContextData } = useGlobalContext();
   return (
     <div>Lets chat
     </div>
