e780178f7ef946ca9fea52954a73b063dc5ecdeb
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-11-15 01:07:33 +0530

    videocall trying

--- CureHealth-FrontEnd/src/pages/VideoCall.jsx
+++ CureHealth-FrontEnd/src/pages/VideoCall.jsx
@@ -1,6 +1,17 @@
-import { Helmet } from 'react-helmet';
+import { useState } from "react";
 
-const VideoCall = () => {
-    return (
-    )
+function App() {
+  const [joined, setJoined] = useState(false);
+  return (
+    <div className='ml-10 flex items-center justify-center'>
+      <div>
+        <h1>Video Call</h1>
+      </div>
+      <div>
+        <button onClick={() => setJoined(true)}>Join Room</button>
+      </div>
+    </div>
+  );
 }
+
+export default App;
