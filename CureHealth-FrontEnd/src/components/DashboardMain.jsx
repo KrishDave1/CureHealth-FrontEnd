f58e614ff29a24ceda3ec368a48ec8c9c25ba32c
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-11-15 19:40:07 +0530

    dashboard Main

--- CureHealth-FrontEnd/src/components/DashboardMain.jsx
+++ CureHealth-FrontEnd/src/components/DashboardMain.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useGlobalContext } from "../context";
 
 const DashboardMain = () => {
+    const { userId } = useGlobalContext();
+
+    let Userdata = async () => {
+    let response = await fetch(
+        `http://127.0.0.1:8000/data/patients/?id=${userId}`
+    );
+    let data = await response.json();
+    console.log(data);
+    };
+    Userdata();
   return (
     <div>DashboardMain</div>
   )
