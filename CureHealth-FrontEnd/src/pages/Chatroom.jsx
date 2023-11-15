44f78bcb4debdbae8c875d85b5d73958c8a10e28
Vaibhav <mittalvaibhav277@gmail.com>
2023-11-15 14:30:59 +0530

    added basic chatroom facillity

--- CureHealth-FrontEnd/src/pages/Chatroom.jsx
+++ CureHealth-FrontEnd/src/pages/Chatroom.jsx
@@ -1,13 +1,55 @@
-import React,{useEffect,useState} from "react";
-import './Chatroom.css';
-import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from 'react-chat-engine-advanced'
-import { useGlobalContext } from "../context";
+import React, { useState } from "react";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+import { ChatEngine, getOrCreateChat } from "react-chat-engine";
+// import { useGlobalContext } from "../context";
 
 const Chatroom = () => {
-  const { ContextData } = useGlobalContext();
+  // const chatProps = useMultiChatLogic(
+  //   "0a0fef24-9ef0-4c24-a0ef-507c2d745aab",
+  //   "Vaibhav24",
+  //   "apple2"
+  // );
+  const [username, setUsername] = useState("");
+
+  function createDirectChat(creds) {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  }
+
+  function renderChatForm(creds) {
+    return (
+      <div>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="shadow-md"
+        />
+        <button onClick={() => createDirectChat(creds)}>Create</button>
+      </div>
+    );
+  }
+  // return (
+  //   <div style={{ height: "100vh" }}>
+  //     <MultiChatWindow {...chatProps} />
+  //     <MultiChatSocket {...chatProps} style={{ height: "100vh" }} />
+  //   </div>
+  // );
   return (
-    <div>Lets chat
-    </div>
+    <ChatEngine
+      height="100vh"
+      userName="Vaibhav24"
+      userSecret="apple2"
+      projectID="0a0fef24-9ef0-4c24-a0ef-507c2d745aab"
+      renderNewChatForm={(creds) => renderChatForm(creds)}
+    />
   );
 };
 
