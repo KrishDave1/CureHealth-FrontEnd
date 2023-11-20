b92b568aecdf78b1d10ab3f278e662d5bb51e9cd
Vaibhav <mittalvaibhav277@gmail.com>
2023-11-20 19:57:23 +0530

    updated appointments page

--- CureHealth-FrontEnd/src/pages/Appointments.jsx
+++ CureHealth-FrontEnd/src/pages/Appointments.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const docTypes = [
@@ -18,46 +18,87 @@ const Appointments = () => {
   const [special, setSpecial] = useState("");
   const [data, setDoctypes] = useState([]);
   const [fetch1, setFetch] = useState(false);
-  async function getDoc() {
+  const getDoc = useCallback(async () => {
     const toSearch = disease;
     for (var i = 0; i < docTypes.length; i++) {
       for (let key in docTypes[i]) {
-        if (docTypes[i][key].indexOf(toSearch) != -1) {
+        if (docTypes[i][key].indexOf(toSearch) !== -1) {
           setSpecial(key);
         }
       }
     }
+  }, [disease]);
+  useEffect(() => {
+    getDoc();
+  }, [getDoc]);
+  const fetchData = useCallback(async () => {
     const res = await fetch(
-      `http://127.0.0.1:8000/data/doctors/?specialization=${special.toString()}`
+      `http://127.0.0.1:8000/data/doctors/?specialization=${special}`
     );
     const data = await res.json();
     setDoctypes(data);
-    console.log(data);
-    setSpecial("");
-    // setDisease("");
-  }
-  useEffect(() => {
-    getDoc();
   }, [special]);
+
+  useEffect(() => {
+    if (special && fetch1) {
+      fetchData();
+      setFetch(false); // Reset fetch1 after fetching data
+    }
+  }, [special, fetch1, fetchData]);
+  const patchData = async () => {
+    const response = await fetch(
+      `http://127.0.0.1:8000/data/doctors/?specialization=${special}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: data[0].id,
+          is_superuser: data[0].is_superuser,
+          password: data[0].password,
+          username: data[0].username,
+          email: data[0].email,
+          phone_number: data[0].phone_number,
+          specialization: data[0].specialization,
+          is_email_verified: data[0].is_email_verified,
+          is_phone_verified: data[0].is_phone_verified,
+          about: data[0].about,
+          is_Free: false,
+          is_active: data[0].is_active,
+          first_name: data[0].first_name,
+          last_name: data[0].last_name,
+        }),
+      }
+    );
+    const data1 = await response.json();
+    console.log(data1);
+  };
+  function handlesubmit(e) {
+    e.preventDefault();
+    setFetch(true);
+
+    getDoc();
+  }
 
   return (
     <div>
       <h1>Appointments</h1>
-      <form>
+      <form onSubmit={handlesubmit}>
         <input
           type="text"
           value={disease}
           placeholder="What is your disease/problem"
           onChange={(e) => setDisease(e.target.value)}
         />
-        <button onClick={getDoc}>Fetch for doctors</button>
+        <button type="submit">Fetch for doctors</button>
         <button
           onClick={() => {
             const bool = window.confirm(
               "Are you sure you want to book an appointment?"
             );
             if (bool) {
-              navigate("/video");
+              patchData();
             }
           }}
         >
@@ -65,16 +106,20 @@ const Appointments = () => {
         </button>
       </form>
       <div>
-        <p>You Should show to a :{special}</p>
-        <h2>We have the following doctors available</h2>
+        {special && data.length > 0 ? (
+          <div className="flex flex-col">
+            <p>You Should show to our speciallised : {special}</p>
+            <h2>We have the following doctors available</h2>
+            <p>Name: {data[0].username}</p>
 
-        <div>
-          {data.map((doc) => (
-            <div key={doc.id}>
-              <p>{doc.email}</p>
-            </div>
-          ))}
-        </div>
+            <p>Email: {data[0].email}</p>
+            <p>About:{data[0].about}</p>
+          </div>
+        ) : (
+          <div>
+            <p>No doctor found</p>
+          </div>
+        )}
       </div>
     </div>
   );
